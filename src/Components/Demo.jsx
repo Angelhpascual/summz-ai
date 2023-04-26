90cf7b66790c6c7ef2b83ba41475c278a258af19
Angelhpascual <Angelhpascual@gmail.com>
2023-04-26 19:52:10 +0200

    Demo Section

--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react"
+import { copy, linkIcon, loader, tick } from "../assets"
+
 const Demo = () => {
-  return <div>Demo</div>
+  return (
+    <section className="w-full max-w-xl mt-16">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex items-center justify-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 w-5 my-2 ml-3"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            ⏎
+          </button>
+        </form>
+        {/* Browse URL History */}
+      </div>
+      {/* Display Results */}
+    </section>
+  )
 }
 
 export default Demo
